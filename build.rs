1f9379543c33edd97fbbbc0e3edb8bed6a76aaf9
g770728y <g770728y@163.com>
2024-01-17 14:36:01 +0800

    fix bug: build.rs

--- build.rs
+++ build.rs
@@ -17,10 +17,7 @@ fn main() {
     // build wrapper
     // 需要在系统环境变量中建两个变量：Library和Lib，打开visual studio，新建一个空项目，然后在项目属性>文件目录中复制这两个变量的内容
     let mut cc_builder = cc::Build::new();
-    cc_builder
-        .file("src/wrapper.cpp")
-        .cpp(true)
-        .std("c++17");
+    cc_builder.file("src/wrapper.cpp").cpp(true).std("c++17");
 
     #[cfg(target_os = "windows")]
     cc_builder.flag("-EHsc");
@@ -31,17 +28,17 @@ fn main() {
         // https://learn.microsoft.com/zh-cn/cpp/build/reference/z7-zi-zi-debug-information-format?view=msvc-170
         // Zi 选项生成一个单独的 PDB 文件，其中包含供调试器使用的所有符号化调试信息
         // 使用 /Zi 不会影响优化。 但是，/Zi 的确表示 /debug
-        build.flag("-Zi");
+        cc_builder.flag("-Zi");
         // https://learn.microsoft.com/zh-cn/cpp/standard-library/iterator-debug-level?view=msvc-170
         // 启用迭代器调试；经过检查的迭代器不相关
         // Clipper2.lib使用了迭代器，所以这里也要开启, 否则link失败
         // 原则上，开启-Zi会默认开启-D_ITERATOR_DEBUG_LEVEL=2，但是这里不知道为什么不行，所以手动开启
-        build.flag("-D_ITERATOR_DEBUG_LEVEL=2");
+        cc_builder.flag("-D_ITERATOR_DEBUG_LEVEL=2");
         // https://learn.microsoft.com/zh-cn/cpp/build/reference/md-mt-ld-use-run-time-library?view=msvc-170
         // 定义 _DEBUG、_MT 和 _DLL，并使此应用程序使用特定于多线程和 DLL 的调试版本的运行库。 它还会让编译器将库名称 MSVCRTD.lib 放入 .obj 文件中。
         // 由于Clipper2.lib是使用MDd编译的，所以这里也要开启, 否则link失败
         // 不太清楚为什么要使用 MDd，而不是 MTd，但是这里也是参考Clipper2
-        build.flag("-MDd");
+        cc_builder.flag("-MDd");
     }
     cc_builder.include("Clipper2/CPP/Clipper2Lib/include");
     cc_builder.compile("clipper2wrap");
